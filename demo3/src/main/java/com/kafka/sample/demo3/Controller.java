ab7300275cb756ea99355caf43d5f6ed0f8ec5cc
ming.wang4 <ming.wang4@l-008645.local>
2021-01-09 18:21:07 +0800

    transaction

---
+++ demo3/src/main/java/com/kafka/sample/demo3/Controller.java
@@ -0,0 +1,33 @@
+package com.kafka.sample.demo3;
+
+import com.kafka.sample.demo3.common.Foo1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @PostMapping(path = "/send/foos/{what}")
+    public void sendFoo(@PathVariable String what) {
+        this.template.executeInTransaction(kafkaTemplate -> {
+            StringUtils.commaDelimitedListToSet(what).stream()
+                    .map(s -> new Foo1(s))
+                    .forEach(foo ->{
+                        if(foo.getFoo().startsWith("f")){
+                            throw new RuntimeException("failed");
+                        }
+                        kafkaTemplate.send("topic2", foo);
+                    });
+            return null;
+        });
+    }
+
+}
+
